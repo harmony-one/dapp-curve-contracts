2af2973c936b4b0ae5656005ffc5c1b0b7cc6067
Daniel Van Der Maden <dvandermaden0@berkeley.edu>
2020-08-05 03:37:25 -0700

    Remove truffle migrations, will use custom deploy script

--- migrations/2_deploy_contracts.js
+++
@@ -1,7 +0,0 @@
-var erc20 = artifacts.require("ERC20");
-
-module.exports = function(deployer) {
-  deployer.deploy(erc20, "curve-test-A", "A", 18, Math.pow(10, 9));
-  deployer.deploy(erc20, "curve-test-B", "B", 18, Math.pow(10, 9));
-  deployer.deploy(erc20, "curve-test-C", "C", 18, Math.pow(10, 9));
-};
