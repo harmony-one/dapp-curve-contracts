db1e1fcae21cbf4e3d3ecf023585d8762fd56dbc
Daniel Van Der Maden <dvandermaden0@berkeley.edu>
2020-08-05 13:51:30 -0700

    Add compile to deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,4 +3,5 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 mkdir -p "$DIR/../build/contracts"
 mkdir -p "$DIR/../deploy/contracts"
+truffle compile
 node "$DIR/deploy.js"
